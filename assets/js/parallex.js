433bd292a20d5621995b07b613f6057b293ef21c
jeeshma prakash s <sjeeshmaprakash@gmail.com>
2025-08-17 10:18:51 +0530

    1

--- assets/js/parallex.js
+++ assets/js/parallex.js
@@ -1,15 +1,15 @@
 /*====== MENU =====*/
-const showMenu = (toggleId,navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+// const showMenu = (toggleId,navId) =>{
+//     const toggle = document.getElementById(toggleId),
+//     nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+//     if(toggle && nav){
+//         toggle.addEventListener('click', ()=>{
+//             nav.classList.toggle('show')
+//         })
+//     }
+// }
+// showMenu('nav-toggle','nav-menu')
 
 /*====== RELLAX =====*/
 var rellax = new Rellax('.parallax');
@@ -17,9 +17,9 @@ var rellax = new Rellax('.parallax');
 
 /*====== ANIMATE GSAP ======*/
 /*Navbar*/
-gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
-gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
-gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
+// gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
+// gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
+// gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
 
 /*Text*/
 gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
